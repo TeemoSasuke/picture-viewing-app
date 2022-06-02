34af7034e6aaaea7de6c0c9b2a0b7024531f7a57
TeemoSasuke <xuanzhuyou@hotmail.com>
2022-06-01 20:27:41 -0400

    Initial Commit

---
+++ pictures_viewer/pictures_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pictures_viewerApp.swift
+//  pictures_viewer
+//
+//  Created by Natalie Wang on 2022-06-01.
+//
+
+import SwiftUI
+
+@main
+struct pictures_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
